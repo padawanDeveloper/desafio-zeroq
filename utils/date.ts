364a03cfdf75f9d7a68bd71ef10d0bb300608197
esteban ortiz <ematiasortiz@gmail.com>
2024-12-04 16:34:09 -0300

    impruved parse date

--- utils/date.ts
+++ utils/date.ts
@@ -1,14 +1,3 @@
 export function minutesToHHMMSS(minutes: number) {
-    let date = new Date(0);
-    date.setMinutes(minutes);
-
-    const hours = date.getUTCHours();
-    const mins = date.getUTCMinutes();
-    const secs = date.getUTCSeconds();
-
-    const formattedTime = `${String(hours).padStart(2, "0")}:${String(
-        mins
-    ).padStart(2, "0")}:${String(secs).padStart(2, "0")}`;
-
-    return formattedTime;
+    return new Date(minutes * 60 * 1000).toISOString().substring(11, 19);
 }
