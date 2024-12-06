a32c2361ca7ad73b666766563b7fcb3c2a7b3114
esteban ortiz <ematiasortiz@gmail.com>
2024-12-05 23:00:27 -0300

    fix head

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,6 @@ export default function Document() {
     return (
         <Html lang="en">
             <Head>
-                <title>ZeroQ</title>
                 <meta name="description" content="Desafio" />
                 <meta
                     name="viewport"
