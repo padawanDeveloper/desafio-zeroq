be1535178999daea01d7c5adca2ce0b2f70d957f
esteban ortiz <ematiasortiz@gmail.com>
2024-12-06 14:32:53 -0300

    fix fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,7 @@ export default function Document() {
                 />
                 <link rel="icon" href="/favicon.ico" />
                 <link
-                    href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp&display=optional"
+                    href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp"
                     rel="stylesheet"
                 />
             </Head>
